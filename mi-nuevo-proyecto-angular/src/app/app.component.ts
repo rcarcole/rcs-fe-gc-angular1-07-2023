49ac4a0d95b4bb17f38a58a4644e92b9708ba01f
rcarcole <rcarcole@hotmail.com>
2023-07-04 19:42:44 +0200

    de momento pilla los numeros y operadores en console.log

--- mi-nuevo-proyecto-angular/src/app/app.component.ts
+++ mi-nuevo-proyecto-angular/src/app/app.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
+import * as math from 'mathjs';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
   title = 'mi-nuevo-proyecto-angular';
+  //  El '!:' significa que la variable puede ser nula en un principio, pero se le asignará un valor en algún momento antes de ser utilizada.
+  result!: number; 
+  pantalla = document.querySelector('#pantalla');
+
+  numero(number: number): void {
+    // Lógica para manejar el número seleccionado
+    console.log("Número seleccionado:", number);
+
+  }
+
+  operador(operador: string) {
+    // Lógica para manejar los operadores
+    console.log("Operador seleccionado:", operador);
+  }
+
+  calcular() {
+    // Lógica para hacer los cálculos
+    try {
+      this.result = math.evaluate(this.result.toString());
+    } catch (error) {
+      console.error("Error en el cálculo:", error);
+    }
+  }
+
+  borrar() {
+    this.result = 0;
+  }
 }
+
+
